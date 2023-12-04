d19f133c30838bf17d42596c037967c63321b4ec
agreenaa <ah5220@rit.edu>
2023-12-04 20:51:32 +0100

    New Class IronLevelTestDecorator added.

---
+++ DecoratorPattern/IronLevelTestDecorator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// IronLevelTestDecorator.cs
+// Concrete decorator adding an Iron Level Test to a blood test.
+namespace DecoratorPattern
+{
+    public class IronLevelTestDecorator : AdditionalTestDecorator
+    {
+        public IronLevelTestDecorator(IBloodTest bloodTest) : base(bloodTest)
+        {
+        }
+
+        public override string GetDescription()
+        {
+            return $"{BloodTest.GetDescription()}, Iron Level Test"; // Appending Iron Level Test to the description.
+        }
+
+        public override decimal GetCost()
+        {
+            return base.GetCost() + 15.0m; // Additional cost for Iron Level Test.
+        }
+    }
+
+}
