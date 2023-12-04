a0d032d7a89a9d91db0f421c70b63027b163e616
agreenaa <ah5220@rit.edu>
2023-12-04 18:46:04 +0100

    Added classes

---
+++ DecoratorPattern/AdditionalTestDecorator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// AdditionalTestDecorator.cs
+// Abstract decorator providing a base for additional test decorators.
+namespace DecoratorPattern
+{
+    public abstract class AdditionalTestDecorator : IBloodTest
+    {
+        protected IBloodTest BloodTest; // Reference to the blood test being decorated.
+
+        public AdditionalTestDecorator(IBloodTest bloodTest)
+        {
+            BloodTest = bloodTest;
+        }
+
+        public virtual string GetDescription()
+        {
+            return BloodTest.GetDescription(); // Delegating description to the wrapped blood test.
+        }
+
+        public virtual decimal GetCost()
+        {
+            return BloodTest.GetCost(); // Delegating cost to the wrapped blood test.
+        }
+    }
+
+}
---
+++ DecoratorPattern/IBloodTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// IBloodTest.cs
+ // Interface representing a blood test.
+namespace DecoratorPattern { 
+    public interface IBloodTest
+    {
+        string GetDescription(); // Get a description of the blood test.
+        decimal GetCost(); // Get the cost of the blood test.
+    }
+}
